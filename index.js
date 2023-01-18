481b2d452ffbb24b173eb0c9768a89a4b9a55982
cnikita26 <nikitachavan3337@gmail.com>
2023-01-18 20:57:36 +0530

    fw22_0301_Day-2

---
+++ index.js
@@ -0,0 +1,15 @@
+function allSet(){
+    return ("BEST OF WINTER SALE FROM $14.99|Details|View All Deals")
+   
+   
+}
+allSet();
+
+setInterval(()=>{
+    return ("SAVE $10,SIGN UP FOR FEMENINE REWARDS TODAY|Details|View All Deals")
+   },2000);
+
+   setInterval(()=>{
+    return ("SPRING PREVIEW UP TO 40% OFF NEW ARRIVALS|Details|View All Deals")
+   },3000);
+
